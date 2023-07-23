da06cbf09fab6630caeb936150e2b78ca461ab40
Maxime-Cottin <max.cttn@gmail.com>
2023-07-24 00:14:47 +0200

    [WIP] Responsive OK

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,162 +6,182 @@ import { createClient } from "@/prismicio";
 import { components } from "@/slices/";
 
 import Image from "next/image";
+import { useEffect, useState } from "react";
+
+import gsap from "gsap";
+
+// Sitemap datas
+const sitemap = [
+	{
+		title: "Product",
+		list: ["Market", "Pricing", "Get the app", "Refer & earn"],
+	},
+	{
+		title: "About us",
+		list: ["Manifest", "We're hiring!", "Legal hub"],
+	},
+	{
+		title: "Academy",
+		list: ["Lorem ipsum", "Lorem ipsum", "Lorem ipsum", "Lorem ipsum"],
+	},
+	{
+		title: "Help",
+		list: ["FAQ", "Community", "contact@diversified.fi"],
+	},
+];
 
 type HomeProps = InferGetStaticPropsType<typeof getStaticProps>;
 
 export default function Index({ home, ctas, carousel }: HomeProps) {
-  return (
-    <main>
-      <Head>
-        <title>{home.data.title}</title>
-      </Head>
-      <SliceZone
-        slices={home.data.slices}
-        components={components}
-        context={{ carousel, ctas }}
-      />
-      <section className="section-footer">
-        <div className="container-footer-top">
-          <div className="footer-top-item">
-            <h6>Product</h6>
-            <ul className="footer-list">
-              <li>Market</li>
-              <li>Pricing</li>
-              <li>Get the app</li>
-              <li>Refer & Earn</li>
-            </ul>
-          </div>
-          <div className="footer-top-item">
-            <h6>About us</h6>
-            <ul className="footer-list">
-              <li>Manifest</li>
-              <li>We're hiring</li>
-              <li>Legal hub</li>
-            </ul>
-          </div>
-          <div className="footer-top-item">
-            <h6>Academy</h6>
-            <ul className="footer-list">
-              <li>Lorem ipsum</li>
-              <li>Lorem ipsum</li>
-              <li>Lorem ipsum</li>
-              <li>Lorem ipsum</li>
-            </ul>
-          </div>
-          <div className="footer-top-item">
-            <h6>Help</h6>
-            <ul className="footer-list">
-              <li>FAQ</li>
-              <li>Community</li>
-              <li>contact@diversifed.fi</li>
-            </ul>
-          </div>
-          <div className="footer-top-item-last">
-            <h6>Sign up for email update</h6>
-            <div className="container-input-mail">
-              <input type="email" className="input-mail" />
-              <button type="submit">Sign up</button>
-            </div>
-            <div className="container-cb">
-              <input type="checkbox" name="cb-mail" />
-              <label htmlFor="cb-mail">
-                Diversified processes your personal data collected via this form
-                for responding to your requests. More informations{" "}
-                <a href="#">Privacy Policy.</a>
-              </label>
-            </div>
-          </div>
-        </div>
-        <div className="container-footer-mid">
-          <p>
-            You invest in Diversified Securitization which will entrust the
-            purchase and management of the assets to a company of the group.
-            Diversified Securitization S.à r.l., a private limited liability
-            company incorporated and existing under the laws of the Grand Duchy
-            of Luxembourg, having its registered office at 20, rue de Hollerich
-            L - 1740 Luxembourg, Grand Duchy of Luxembourg and registered with
-            the Luxembourg Trade and Companies Register under number
-            B274704*Past performance is no guarantee of future performance. Any
-            investment carries the risk of total or partial capital loss.
-            Diversified invites you to do your own research before investing.
-          </p>
-        </div>
-        <div className="container-footer-bot">
-          <div className="container-logo">
-            <Image
-              src={"/assets/Logo-diversified-black.svg"}
-              alt=""
-              width={200}
-              height={38}
-            />
-            <p>©2023 Diversified. All rights Reserved.</p>
-          </div>
-          <div className="container-catchphrase">
-            <p>
-              Invest in ultra-rare and profitable assets like wine, whisky,
-              watches, bags and art.
-            </p>
-          </div>
-          <div className="container-socials">
-            <p>Join us</p>
-            <div className="container-socials-icons">
-              <Image
-                src={"/assets/Discord.svg"}
-                alt=""
-                width={14}
-                height={14}
-              />
-              <Image
-                src={"/assets/LinkedIn.svg"}
-                alt=""
-                width={14}
-                height={14}
-              />
-              <Image
-                src={"/assets/Facebook.svg"}
-                alt=""
-                width={14}
-                height={14}
-              />
-              <Image
-                src={"/assets/Twitter.svg"}
-                alt=""
-                width={14}
-                height={14}
-              />
-              <Image
-                src={"/assets/Instagram.svg"}
-                alt=""
-                width={14}
-                height={14}
-              />
-              <Image
-                src={"/assets/Youtube.svg"}
-                alt=""
-                width={14}
-                height={14}
-              />
-            </div>
-          </div>
-        </div>
-      </section>
-    </main>
-  );
+	return (
+		<main>
+			<Head>
+				<title>{home.data.title}</title>
+			</Head>
+			<SliceZone
+				slices={home.data.slices}
+				components={components}
+				context={{ carousel, ctas }}
+			/>
+			<section id="section-footer">
+				<div className="container-footer-top">
+					<div className="footer-sitemap">
+						{sitemap.map((item, key) => (
+							<div key={key} className="footer-sitemap-item">
+								<div className="sitemap-item-title">
+									<h6>{item.title}</h6>
+									<div className={`question-tab-icon question-tab-icon-${key}`}>
+										<svg
+											width="11"
+											height="6"
+											viewBox="0 0 11 6"
+											fill="none"
+											xmlns="http://www.w3.org/2000/svg"
+										>
+											<path
+												fill-rule="evenodd"
+												clip-rule="evenodd"
+												d="M5.88867 6.55671e-07L10.8887 4.80077L9.63768 6L5.88867 2.40038L2.13967 5.99999L0.888672 4.80076L5.88867 6.55671e-07Z"
+												fill="white"
+											/>
+										</svg>
+									</div>
+								</div>
+
+								<ul className={`sitemap-list sitemap-list-${key}`}>
+									{item.list.map((listItem) => (
+										<li>{listItem}</li>
+									))}
+								</ul>
+							</div>
+						))}
+					</div>
+					<div className="footer-newsletter">
+						<h6>Sign up for email update</h6>
+						<div className="container-input-mail">
+							<input type="email" className="input-mail" />
+							<button type="submit">Sign up</button>
+						</div>
+						<div className="container-cb">
+							<input type="checkbox" name="cb-mail" />
+							<label htmlFor="cb-mail">
+								Diversified processes your personal data collected via this form
+								for responding to your requests. More informations{" "}
+								<a href="#">Privacy Policy.</a>
+							</label>
+						</div>
+					</div>
+				</div>
+				<div className="container-footer-mid">
+					<p>
+						You invest in Diversified Securitization which will entrust the
+						purchase and management of the assets to a company of the group.
+						Diversified Securitization S.à r.l., a private limited liability
+						company incorporated and existing under the laws of the Grand Duchy
+						of Luxembourg, having its registered office at 20, rue de Hollerich
+						L - 1740 Luxembourg, Grand Duchy of Luxembourg and registered with
+						the Luxembourg Trade and Companies Register under number
+						B274704*Past performance is no guarantee of future performance. Any
+						investment carries the risk of total or partial capital loss.
+						Diversified invites you to do your own research before investing.
+					</p>
+				</div>
+				<div className="container-footer-bot">
+					<div className="container-logo">
+						<Image
+							src={"/assets/Logo-diversified-black.svg"}
+							alt=""
+							width={200}
+							height={38}
+						/>
+						<p>©2023 Diversified. All rights Reserved.</p>
+					</div>
+					<div className="container-catchphrase">
+						<p>
+							Invest in ultra-rare and profitable assets like wine, whisky,
+							watches, bags and art.
+						</p>
+					</div>
+					<div className="container-socials">
+						<p>Join us</p>
+						<div className="container-socials-icons">
+							<Image
+								src={"/assets/Discord.svg"}
+								alt=""
+								width={14}
+								height={14}
+							/>
+							<Image
+								src={"/assets/LinkedIn.svg"}
+								alt=""
+								width={14}
+								height={14}
+							/>
+							<Image
+								src={"/assets/Facebook.svg"}
+								alt=""
+								width={14}
+								height={14}
+							/>
+							<Image
+								src={"/assets/Twitter.svg"}
+								alt=""
+								width={14}
+								height={14}
+							/>
+							<Image
+								src={"/assets/Instagram.svg"}
+								alt=""
+								width={14}
+								height={14}
+							/>
+							<Image
+								src={"/assets/Youtube.svg"}
+								alt=""
+								width={14}
+								height={14}
+							/>
+						</div>
+					</div>
+				</div>
+			</section>
+		</main>
+	);
 }
 
 export async function getStaticProps({ previewData }: GetStaticPropsContext) {
-  const client = createClient({ previewData });
+	const client = createClient({ previewData });
 
-  const home = await client.getSingle("home");
+	const home = await client.getSingle("home");
 
-  const ctas = await client.getAllByType("cta");
-  const carousel = await client.getAllByType("gallery_carousel");
+	const ctas = await client.getAllByType("cta");
+	const carousel = await client.getAllByType("gallery_carousel");
 
-  return {
-    props: {
-      home,
-      carousel,
-      ctas,
-    },
-  };
+	return {
+		props: {
+			home,
+			carousel,
+			ctas,
+		},
+	};
 }
--- src/slices/GetApp/index.tsx
+++ src/slices/GetApp/index.tsx
@@ -16,81 +16,85 @@ import gsap from "gsap";
 export type GetAppProps = SliceComponentProps<Content.GetAppSlice>;
 
 const GetApp = ({ slice }: GetAppProps): JSX.Element => {
-  useEffect(() => {
-    gsap.set(".get-app", {
-      x: (i) => i * 650,
-    });
+	useEffect(() => {
+		gsap.set(".get-app", {
+			x: (i) => i * 650,
+		});
 
-    gsap.to(".get-app", {
-      duration: 5,
-      ease: "none",
-      x: "-=650",
-      modifiers: {
-        y: gsap.utils.unitize((y) => parseFloat(y) % 650),
-      },
-      repeat: -1,
-    });
-  }, []);
+		gsap.to(".get-app", {
+			duration: 5,
+			ease: "none",
+			x: "-=650",
+			modifiers: {
+				y: gsap.utils.unitize((y) => parseFloat(y) % 650),
+			},
+			repeat: -1,
+		});
+	}, []);
 
-  return (
-    <section
-      data-slice-type={slice.slice_type}
-      data-slice-variation={slice.variation}
-      id="section-get-app"
-    >
-      <div className="wrapper wrapper-get-app">
-        <div className="slider">
-          <div className="slider-container">
-            <div className="get-app">
-              <PrismicRichText field={slice.primary.title} />
-              <PrismicNextImage field={slice.primary.qr_code} />
-            </div>
-            <div className="get-app">
-              <PrismicRichText field={slice.primary.title} />
-              <PrismicNextImage field={slice.primary.qr_code} />
-            </div>
-            <div className="get-app">
-              <PrismicRichText field={slice.primary.title} />
-              <PrismicNextImage field={slice.primary.qr_code} />
-            </div>
-            <div className="get-app">
-              <PrismicRichText field={slice.primary.title} />
-              <PrismicNextImage field={slice.primary.qr_code} />
-            </div>
-          </div>
-        </div>
-        <div className="store-links">
-          <div className="ios">
-            <p className="title">iOS App Store</p>
-            <p className="note">⭐️⭐️⭐️⭐️⭐️</p>
-            <p className="reviews">4,5 stars on 453 rates</p>
-            <PrismicNextLink field={slice.primary.android_app}>
-              <Image
-                src={"/assets/ios-download.png"}
-                width={120}
-                height={40}
-                alt="Lorem ipsum"
-              />
-            </PrismicNextLink>
-          </div>
-          <div className="divider"></div>
-          <div className="google">
-            <p className="title">Play Store</p>
-            <p className="note">⭐️⭐️⭐️⭐️⭐️</p>
-            <p className="reviews">4,5 stars on 453 rates</p>
-            <PrismicNextLink field={slice.primary.android_app}>
-              <Image
-                src={"/assets/google-download.png"}
-                width={120}
-                height={40}
-                alt="Lorem ipsum"
-              />
-            </PrismicNextLink>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+	return (
+		<section
+			data-slice-type={slice.slice_type}
+			data-slice-variation={slice.variation}
+			id="section-get-app"
+		>
+			<div className="wrapper wrapper-get-app">
+				<div className="slider">
+					<div className="slider-container">
+						<div className="get-app">
+							<PrismicRichText field={slice.primary.title} />
+							<PrismicNextImage field={slice.primary.qr_code} />
+						</div>
+						<div className="get-app">
+							<PrismicRichText field={slice.primary.title} />
+							<PrismicNextImage field={slice.primary.qr_code} />
+						</div>
+						<div className="get-app">
+							<PrismicRichText field={slice.primary.title} />
+							<PrismicNextImage field={slice.primary.qr_code} />
+						</div>
+						<div className="get-app">
+							<PrismicRichText field={slice.primary.title} />
+							<PrismicNextImage field={slice.primary.qr_code} />
+						</div>
+						<div className="get-app">
+							<PrismicRichText field={slice.primary.title} />
+							<PrismicNextImage field={slice.primary.qr_code} />
+						</div>
+					</div>
+				</div>
+				<div className="store-links">
+					<div className="ios">
+						<p className="title">iOS App Store</p>
+						<p className="note">⭐️⭐️⭐️⭐️⭐️</p>
+						<p className="reviews">4,5 stars on 453 rates</p>
+						<PrismicNextLink field={slice.primary.android_app}>
+							<Image
+								src={"/assets/ios-download.png"}
+								width={120}
+								height={40}
+								alt="Lorem ipsum"
+							/>
+						</PrismicNextLink>
+					</div>
+					<div className="divider"></div>
+					<div className="google">
+						<p className="title">Play Store</p>
+						<p className="note">⭐️⭐️⭐️⭐️⭐️</p>
+						<p className="reviews">4,5 stars on 453 rates</p>
+						<PrismicNextLink field={slice.primary.android_app}>
+							<Image
+								src={"/assets/google-download.png"}
+								width={120}
+								height={40}
+								alt="Lorem ipsum"
+							/>
+						</PrismicNextLink>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default GetApp;
